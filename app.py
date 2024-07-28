517bb85ff52c54d06a609d652598106547be157e
Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-07-28 17:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-from nltk.stem.porter import PorterStemmer
+from nltk.stem.porter import PorterStemmer 
 
 ps = PorterStemmer()
 
